38004a8363a901d05314d3bcf669c05cc1eaedba
Akshay Kumar K M <158058962+akshaykumarkm901@users.noreply.github.com>
2025-09-29 21:32:16 +0530

    Update listing.js
    image: CastError: Cast to string failed for value

--- models/listing.js
+++ models/listing.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const listingSchema = new Schema({
+const listingSchema = Schema({
   title: {
     type: String,
     required: true,
@@ -9,17 +9,18 @@ const listingSchema = new Schema({
   description: String,
   image: {
     type: String,
-    default:
-      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-    set: (v) =>
-      v === ""
-        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
-        : v,
+    default: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/681399313.jpg?k=ce6690d3d0c2d37ff2c19f253ed4b44c623dfa019162f19b6b2754e5b43c9e56&o=&hp=1",
+    set: (v) => {
+      // If frontend sends { url: "..." } → extract string
+      if (typeof v === "object" && v?.url) return v.url;
+      // If empty string, return default
+      if (v === "") return "https://cf.bstatic.com/xdata/images/hotel/max1024x768/681399313.jpg?k=ce6690d3d0c2d37ff2c19f253ed4b44c623dfa019162f19b6b2754e5b43c9e56&o=&hp=1";
+      return v;
+    }
   },
   price: Number,
   location: String,
   country: String,
 });
-
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
